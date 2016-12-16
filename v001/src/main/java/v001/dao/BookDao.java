73f4b2e5c48bbb04e11c15383f190eadf709eed7
Stanislav Leontiev <s123s@inbox.ru>
2016-12-16 10:54:21 +0200

    Correction of v002

---
+++ v001/src/main/java/v001/dao/BookDao.java
@@ -0,0 +1,6 @@
+package v001.dao;
+
+import model.Book;
+
+public interface BookDao extends DaoDefault<Book, Integer>{
+}
---
+++ v001/src/main/java/v001/service/BookService.java
@@ -0,0 +1,10 @@
+package v001.service;
+
+import java.util.List;
+
+import v001.dao.DaoDefault;
+import v001.model.Book;
+
+public interface BookService extends DaoDefault<Book, Integer> {
+
+}
